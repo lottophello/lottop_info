a96fe0c1e66a3765c36f5479979d69598c5dcc45
lottophello <160763666+lottophello@users.noreply.github.com>
2024-07-25 19:30:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var Misha= document.getElementById('cat')
+let theme = false;
+let first = false;
+let sec = false;
+function First_que(){
+    window.globalThisfirst = true;
+    console.log(first);
+}
+First_que();
+console.log(first);
